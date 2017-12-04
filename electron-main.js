31b9bc6e8766c8f4557624de123f41a7fe247f4b
Will Vincent <will@willvincent.com>
2017-12-04 02:10:34 -0600

    Update electron-main.js
    electron build script doesn't properly include things for the about window to work. Apparently I missed excluding this `require` before my last commit. :\

--- electron-main.js
+++ electron-main.js
@@ -1,6 +1,6 @@
 const electron = require('electron');
 const path = require('path');
-const openAboutWindow = require('about-window').default;
+// const openAboutWindow = require('about-window').default;
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
